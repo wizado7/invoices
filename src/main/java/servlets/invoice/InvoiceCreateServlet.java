cff1c7791db5b5ade3ab108875a3517b730fc84c
wizado <wizado7@gmail.com>
2024-12-02 02:30:59 +0300

    added doGet method for create invoices

--- src/main/java/servlets/invoice/InvoiceCreateServlet.java
+++ src/main/java/servlets/invoice/InvoiceCreateServlet.java
@@ -1,8 +1,14 @@
 package servlets.invoice;
 
 import Interfaces.DAL.InvoiceDAO;
+import Interfaces.DAL.ItemDAO;
 import config.ConnectionManager;
+import entity.Firm;
+import entity.Invoice;
+import entity.InvoiceItem;
+import entity.Item;
 import impl.InvoiceDAOImpl;
+import impl.ItemDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,10 +18,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 
 @WebServlet("/invoices/create")
 public class InvoiceCreateServlet extends HttpServlet {
     private InvoiceDAO invoiceDAO;
+    private ItemDAO itemDAO;
 
     @Override
     public void init() {
@@ -23,6 +32,7 @@ public class InvoiceCreateServlet extends HttpServlet {
         try {
             connection = ConnectionManager.getConnection();
             invoiceDAO = new InvoiceDAOImpl(connection);
+            itemDAO = new ItemDAOImpl(connection);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -30,6 +40,19 @@ public class InvoiceCreateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            List<Item> items = itemDAO.getAllItems();
+
+            req.setAttribute("items", items);
+            req.getRequestDispatcher("/invoice-create.jsp").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Ошибка при загрузке страницы создания накладной");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
 }
