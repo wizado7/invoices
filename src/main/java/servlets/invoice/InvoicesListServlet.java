029caecd8925f2c8d8df141021d69c60efedf2ca
wizado <wizado7@gmail.com>
2024-12-02 00:31:35 +0300

    create InvoicesListServlet, init

---
+++ src/main/java/servlets/invoice/InvoicesListServlet.java
@@ -0,0 +1,33 @@
+package servlets.invoice;
+
+import Interfaces.DAL.InvoiceDAO;
+import config.ConnectionManager;
+import impl.InvoiceDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class InvoicesListServlet extends HttpServlet {
+    private InvoiceDAO invoiceDAO;
+
+    @Override
+    public void init() {
+        Connection connection = null;
+        try {
+            connection = ConnectionManager.getConnection();
+            invoiceDAO = new InvoiceDAOImpl(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
