2a8925487547347c35d35f2756c4a4e45321f792
wizado <wizado7@gmail.com>
2024-12-02 01:46:54 +0300

    added servlet for create invoices

---
+++ src/main/java/servlets/invoice/InvoiceCreateServlet.java
@@ -0,0 +1,35 @@
+package servlets.invoice;
+
+import Interfaces.DAL.InvoiceDAO;
+import config.ConnectionManager;
+import impl.InvoiceDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/invoices/create")
+public class InvoiceCreateServlet extends HttpServlet {
+    private InvoiceDAO invoiceDAO;
+
+    @Override
+    public void init() {
+        Connection connection = null;
+        try {
+            connection = ConnectionManager.getConnection();
+            invoiceDAO = new InvoiceDAOImpl(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
