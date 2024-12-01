af9c76ca32be6c8b8897b4709c9f289cf66016dc
wizado <wizado7@gmail.com>
2024-12-02 02:16:03 +0300

    add init

--- src/main/java/servlets/invoice/InvoiceUpdateServlet.java
+++ src/main/java/servlets/invoice/InvoiceUpdateServlet.java
@@ -1,11 +1,13 @@
 package servlets.invoice;
 
 
+import Interfaces.DAL.InvoiceDAO;
 import config.ConnectionManager;
 import entity.Firm;
 import entity.Invoice;
 import entity.InvoiceItem;
 import entity.Item;
+import impl.InvoiceDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -14,6 +16,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
@@ -21,6 +24,18 @@ import java.util.Map;
 
 @WebServlet("/invoices/update")
 public class InvoiceUpdateServlet extends HttpServlet {
+    private InvoiceDAO invoiceDAO;
+
+    @Override
+    public void init() {
+        Connection connection = null;
+        try {
+            connection = ConnectionManager.getConnection();
+            invoiceDAO = new InvoiceDAOImpl(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
