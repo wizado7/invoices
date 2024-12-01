b2962945cbe3929dcf14432a5ebb97fd2cf2d82c
wizado <wizado7@gmail.com>
2024-12-01 21:57:34 +0300

    Revert "added implementation"
    This reverts commit 158544ac0b39beff9546751c4b000cb317ab6699.

--- src/main/java/impl/InvoiceItemDAOImpl.java
+++ src/main/java/impl/InvoiceItemDAOImpl.java
@@ -1,85 +1,27 @@
 package impl;
 
-
 import Interfaces.DAL.InvoiceItemDAO;
 import entity.InvoiceItem;
 
-import java.sql.*;
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 
 public class InvoiceItemDAOImpl implements InvoiceItemDAO {
-    private final Connection connection;
-
-    public InvoiceItemDAOImpl(Connection connection) {
-        this.connection = connection;
-    }
-
     @Override
     public void addInvoiceItem(InvoiceItem invoiceItem) {
-        String sql = "INSERT INTO invoice_items (invoice_id, item_id, quantity) VALUES (?, ?, ?)";
-        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            stmt.setLong(1, invoiceItem.getInvoiceId());
-            stmt.setLong(2, invoiceItem.getItemId());
-            stmt.setInt(3, invoiceItem.getQuantity());
-            stmt.executeUpdate();
 
-            ResultSet rs = stmt.getGeneratedKeys();
-            if (rs.next()) {
-                invoiceItem.setId(rs.getLong(1));
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public InvoiceItem getInvoiceItemByInvoiceAndItemId(int invoiceId, int itemId) {
-        String sql = "SELECT * FROM invoice_items WHERE invoice_id = ? AND item_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, invoiceId);
-            statement.setInt(2, itemId);
-
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    InvoiceItem invoiceItem = new InvoiceItem();
-                    invoiceItem.setId(resultSet.getInt("id"));
-                    invoiceItem.setInvoiceId(resultSet.getInt("invoice_id"));
-                    invoiceItem.setItemId(resultSet.getInt("item_id"));
-                    invoiceItem.setQuantity(resultSet.getInt("quantity"));
-                    return invoiceItem;
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 
     @Override
     public List<InvoiceItem> getInvoiceItemsByInvoiceId(int invoiceId) {
-        List<InvoiceItem> invoiceItems = new ArrayList<>();
-        String sql = "SELECT * FROM invoice_items WHERE invoice_id = ?";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, invoiceId);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                InvoiceItem invoiceItem = new InvoiceItem();
-                invoiceItem.setInvoiceId(rs.getInt("invoice_id"));
-                invoiceItem.setItemId(rs.getInt("item_id"));
-                invoiceItem.setQuantity(rs.getInt("quantity"));
-                invoiceItems.add(invoiceItem);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return invoiceItems;
+        return null;
     }
 
     @Override
-    public void deleteInvoiceItemsByInvoiceId(int invoiceId) {
+    public void deleteInvoiceItemsByInvoiceId(int invoice) {
 
     }
+
     @Override
     public void updateQuantity(int invoiceId, int itemId, int quantity) {
 
@@ -87,45 +29,26 @@ public class InvoiceItemDAOImpl implements InvoiceItemDAO {
 
     @Override
     public void updateInvoiceItem(InvoiceItem invoiceItem) {
-        String sql = "UPDATE invoice_items SET quantity = ? WHERE invoice_id = ? AND item_id = ?";
-        try (PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setInt(1, invoiceItem.getQuantity());
-            statement.setInt(2, invoiceItem.getInvoiceId());
-            statement.setInt(3, invoiceItem.getItemId());
 
-            int rowsUpdated = statement.executeUpdate();
-            System.out.println("InvoiceItem обновлен: " + rowsUpdated + " строки");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
-
     @Override
-    public void updateInvoiceItem(int invoiceId, int itemId, int quantity) throws SQLException {
+    public void deleteInvoiceItem(long id) {
 
     }
 
     @Override
     public void deleteInvoiceItem(int invoiceId, int itemId) throws SQLException {
-        String sql = "DELETE FROM invoice_items WHERE invoice_id = ? AND item_id = ?";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, invoiceId);
-            stmt.setInt(2, itemId);
-            stmt.executeUpdate();
-        }
+
     }
 
     @Override
-    public void deleteInvoiceItem(long id) {
-        String sql = "DELETE FROM invoice_items WHERE invoice_id = ?";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setLong(1, id);
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    public void updateInvoiceItem(int invoiceId, int id, int quantity) throws SQLException {
+
+    }
+
+    @Override
+    public InvoiceItem getInvoiceItemByInvoiceAndItemId(int invoiceId, int itemId) {
+        return null;
     }
 }
-
-
