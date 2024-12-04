5c4ee2a1677e0512566c3a38851a74f6d7fa38a5
wizado <wizado7@gmail.com>
2024-12-04 04:45:06 +0300

    added firm servlets

---
+++ src/main/java/servlets/firm/FirmCreateServlet.java
@@ -0,0 +1,26 @@
+package servlets.firm;
+
+import Interfaces.DAL.FirmDAO;
+import config.ConnectionManager;
+import impl.FirmDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/firms/create")
+public class FirmCreateServlet extends HttpServlet {
+    private FirmDAO firmDAO;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            Connection connection = ConnectionManager.getConnection();
+            this.firmDAO = new FirmDAOImpl(connection);
+        } catch (SQLException e) {
+            throw new ServletException("Ошибка подключения к базе данных, e");
+        }
+    }
+}
