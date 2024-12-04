08509b006a4e505ad858eb1359ad6ee9d7f80fa9
wizado <wizado7@gmail.com>
2024-12-04 04:50:09 +0300

    added doGet for firm update

--- src/main/java/servlets/firm/FirmUpdateServlet.java
+++ src/main/java/servlets/firm/FirmUpdateServlet.java
@@ -2,11 +2,15 @@ package servlets.firm;
 
 import Interfaces.DAL.FirmDAO;
 import config.ConnectionManager;
+import entity.Firm;
 import impl.FirmDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -21,6 +25,35 @@ public class FirmUpdateServlet extends HttpServlet {
             this.firmDAO = new FirmDAOImpl(connection);
         } catch (SQLException e) {
             throw new ServletException("Ошибка подключения к базе данных, e");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int firmId;
+        try {
+            firmId = Integer.parseInt(req.getParameter("id"));
+        } catch (NumberFormatException | NullPointerException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Некорректный ID фирмы");
+            return;
+        }
+
+        try (Connection connection = ConnectionManager.getConnection()) {
+            FirmDAO firmDAO = new FirmDAOImpl(connection);
+
+
+            Firm firm = firmDAO.getFirmById(firmId);
+            if (firm == null) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Фирма с ID " + firmId + " не найден");
+                return;
+            }
+
+
+            req.setAttribute("firm", firm);
+            req.getRequestDispatcher("/firm-update.jsp").forward(req, resp);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Ошибка базы данных");
         }
     }
 
