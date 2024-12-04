9fefd4a0e626da691c4341c525c4d1d63913250a
wizado <wizado7@gmail.com>
2024-12-04 04:47:38 +0300

    added doGet n doPost methods for firm create

--- src/main/java/servlets/firm/FirmCreateServlet.java
+++ src/main/java/servlets/firm/FirmCreateServlet.java
@@ -2,11 +2,15 @@ package servlets.firm;
 
 import Interfaces.DAL.FirmDAO;
 import config.ConnectionManager;
+import entity.Firm;
 import impl.FirmDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -21,6 +25,35 @@ public class FirmCreateServlet extends HttpServlet {
             this.firmDAO = new FirmDAOImpl(connection);
         } catch (SQLException e) {
             throw new ServletException("Ошибка подключения к базе данных, e");
+        }
+    }
+
+    @Override
+    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/firm-create.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            String firmName = request.getParameter("firmName");
+            String firmAddress = request.getParameter("firmAddress");
+            String firmPhone = request.getParameter("firmPhone");
+
+            if (firmName == null || firmAddress == null || firmPhone == null || firmName.isEmpty() || firmAddress.isEmpty() || firmPhone.isEmpty()) {
+                request.setAttribute("error", "Все поля обязательны для заполнения");
+                request.getRequestDispatcher("/firm-create.jsp").forward(request, response);
+            }
+
+            Firm newFirm = new Firm();
+            newFirm.setName(firmName);
+            newFirm.setAddress(firmAddress);
+            newFirm.setPhone(firmPhone);
+
+            firmDAO.addFirm(newFirm);
+            response.sendRedirect("/invoices");
+        } catch (Exception e) {
+            throw new ServletException("Ошибка добавления новой фирмы");
         }
     }
 }
