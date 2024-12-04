470adb91defd8f49cf790874a406a2b8736ffed0
wizado <wizado7@gmail.com>
2024-12-04 04:48:59 +0300

    added doGet for firm delete

--- src/main/java/servlets/firm/FirmDeleteServlet.java
+++ src/main/java/servlets/firm/FirmDeleteServlet.java
@@ -6,7 +6,10 @@ import impl.FirmDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -21,6 +24,20 @@ public class FirmDeleteServlet extends HttpServlet {
             this.firmDAO = new FirmDAOImpl(connection);
         } catch (SQLException e) {
             throw new ServletException("Ошибка подключения к базе данных, e");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (Connection connection = ConnectionManager.getConnection()) {
+            FirmDAOImpl firmDAO = new FirmDAOImpl(connection);
+            long id = Long.parseLong(req.getParameter("id"));
+            firmDAO.deleteFirm(id);
+
+            resp.sendRedirect("/invoices");
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Ошибка удаления фирмы");
         }
     }
 }
