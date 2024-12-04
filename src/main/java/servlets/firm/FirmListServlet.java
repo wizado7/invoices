5f3964af3931e9b8a0818453ae93ed4748731b0e
wizado <wizado7@gmail.com>
2024-12-04 04:46:18 +0300

    added doGet method for firm list

--- src/main/java/servlets/firm/FirmListServlet.java
+++ src/main/java/servlets/firm/FirmListServlet.java
@@ -2,13 +2,18 @@ package servlets.firm;
 
 import Interfaces.DAL.FirmDAO;
 import config.ConnectionManager;
+import entity.Firm;
 import impl.FirmDAOImpl;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet("/firms")
 public class FirmListServlet extends HttpServlet {
@@ -23,4 +28,18 @@ public class FirmListServlet extends HttpServlet {
             throw new ServletException("Ошибка подключения к базе данных, e");
         }
     }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try  {
+            List<Firm> firms = firmDAO.getAllFirms();
+            req.setAttribute("firms", firms);
+
+            req.getRequestDispatcher("/firms.jsp").forward(req, resp);
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Ошибка при загрузке накладных");
+        }
+    }
+
 }
