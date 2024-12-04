f601080e268a0c6ad127d8139aaacf53cb02608f
wizado <wizado7@gmail.com>
2024-12-04 13:34:35 +0300

    added item servlets

---
+++ src/main/java/servlets/item/ItemSearchServlet.java
@@ -0,0 +1,36 @@
+package servlets.item;
+
+
+
+import Interfaces.DAL.ItemDAO;
+import config.ConnectionManager;
+import entity.Item;
+import impl.ItemDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet("/items/search")
+public class ItemSearchServlet extends HttpServlet {
+    private ItemDAO itemDAO;
+
+    @Override
+    public void init() {
+        Connection connection = null;
+        try {
+            connection = ConnectionManager.getConnection();
+            itemDAO = new ItemDAOImpl(connection);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+}
---
+++ src/main/java/servlets/item/ItemUpdateServlet.java
@@ -0,0 +1,35 @@
+package servlets.item;
+
+
+
+import Interfaces.DAL.ItemDAO;
+import config.ConnectionManager;
+import entity.Item;
+import impl.ItemDAOImpl;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@WebServlet("/items/update")
+public class ItemUpdateServlet extends HttpServlet {
+
+    private ItemDAO itemDAO;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            Connection connection = ConnectionManager.getConnection();
+            this.itemDAO = new ItemDAOImpl(connection);
+        } catch (SQLException e) {
+            throw new ServletException("Ошибка подключения к базе данных", e);
+        }
+    }
+
+
+}
